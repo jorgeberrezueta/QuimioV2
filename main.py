40f3ba75887ce025909739f24e13a7afcd40d046
jorgeberrezueta <jorgeberrezuetav@gmail.com>
2022-04-01 15:36:03 -0500

    Abstraer modelo de regresion

--- main.py
+++ main.py
@@ -2,7 +2,8 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import Lasso, LinearRegression
+import os
 
 from plot import plot_decision_regions
 
@@ -48,57 +49,69 @@ X = c_espe_norm.values
 # Composiciones completas normalizadas
 y = c_comp_norm
 
-lr = LinearRegression()
-
-# Entrenar unicamente utilizando los espectros normalizados (x) y composiciones normalizadas (y) completos
-lr.fit(X, y)
-
-# Obtener una estimacion utilizando todos los espectros normalizados
-estimaciones = pd.DataFrame(columns=["est_C/G", "est_G/N", "est_P/G"], data=lr.predict(t_espe_norm))
-estimaciones.insert(loc=0, column="WL", value=datos["WL"])
-estimaciones.to_csv('output/estimaciones.csv', index=False)
-
-#############################################
-### REEMPLAZAR VALORES EN LA MATRIZ ORIGINAL
-#############################################
-
-combinado = datos.copy()
-
-indices_vacios = combinado[combinado["C/G"].isna()].index
-
-combinado['STATE'] = 'original'
-combinado.loc[combinado["C/G"].isna(), 'STATE'] = 'estimacion'
-combinado.loc[combinado["C/G"].isna(), 'C/G'] = estimaciones.values[:, 0][indices_vacios]
-combinado.loc[combinado["G/N"].isna(), 'G/N'] = estimaciones.values[:, 1][indices_vacios]
-combinado.loc[combinado["P/G"].isna(), 'P/G'] = estimaciones.values[:, 2][indices_vacios]
-
-combinado = combinado[["WL", "STATE", "C/G", "G/N", "P/G"]]
-
-print(combinado)
-
-combinado.to_csv('output/combinado.csv', index=False)
-
-#############
-### GRAFICAR
-#############
-
-# Graficar X = G/N, Y = C/G, color = STATE
-
-plt.scatter(
-    combinado[combinado["STATE"] == 'original']['G/N'],
-    combinado[combinado["STATE"] == "original"]["P/G"],
-    c='blue',
-)
-plt.scatter(
-    combinado[combinado["STATE"] == 'estimacion']['G/N'],
-    combinado[combinado["STATE"] == "estimacion"]["P/G"],
-    c='red',
-)
-
-plt.xlabel('G/N')
-plt.ylabel('P/G')
-
-plt.show()
-
 # plt.matshow(t_espe_norm)
 # plt.show()
+
+class WrapperModelo():
+    # Crear constructor
+    def __init__(self, nombre, model):
+        self.nombre = nombre
+        self.model = model
+
+    def fit(self, X, y):
+        self.model.fit(X, y)
+
+    def predict(self, X):
+        return self.model.predict(X)
+
+    def ejecutar(self):
+        if os.path.exists(f'output/{self.nombre}') is False:
+            os.makedirs(f'output/{self.nombre}')
+        # Obtener una estimacion utilizando todos los espectros normalizados
+        self.estimaciones = pd.DataFrame(columns=["est_C/G", "est_G/N", "est_P/G"], data=self.predict(t_espe_norm))
+        self.estimaciones.insert(loc=0, column="WL", value=datos["WL"])
+        self.estimaciones.to_csv(f'output/{self.nombre}/estimaciones.csv', index=False)
+        #############################################
+        ### REEMPLAZAR VALORES EN LA MATRIZ ORIGINAL
+        #############################################
+        self.combinado = datos.copy()
+        self.indices_vacios = self.combinado[self.combinado["C/G"].isna()].index
+        self.combinado['STATE'] = 'original'
+        self.combinado.loc[self.combinado["C/G"].isna(), 'STATE'] = 'estimacion'
+        self.combinado.loc[self.combinado["C/G"].isna(), 'C/G'] = self.estimaciones.values[:, 1][self.indices_vacios]
+        self.combinado.loc[self.combinado["G/N"].isna(), 'G/N'] = self.estimaciones.values[:, 2][self.indices_vacios]
+        self.combinado.loc[self.combinado["P/G"].isna(), 'P/G'] = self.estimaciones.values[:, 3][self.indices_vacios]
+        self.combinado = self.combinado[["WL", "STATE", "C/G", "G/N", "P/G"]]
+        print(self.combinado)
+        self.combinado.to_csv(f'output/{self.nombre}/combinado.csv', index=False)
+
+    def graficar(self):
+        #############
+        ### GRAFICAR
+        #############
+        # Graficar X = G/N, Y = C/G, color = STATE
+        plt.scatter(
+            self.combinado[self.combinado["STATE"] == 'original']['G/N'],
+            self.combinado[self.combinado["STATE"] == "original"]["P/G"],
+            c='blue',
+        )
+        plt.scatter(
+            self.combinado[self.combinado["STATE"] == 'estimacion']['G/N'],
+            self.combinado[self.combinado["STATE"] == "estimacion"]["P/G"],
+            c='red',
+        )
+        plt.xlabel('G/N')
+        plt.ylabel('P/G')
+        plt.show()
+
+modelos = {
+    "linear": LinearRegression(),
+    "lasso": Lasso(alpha=0.1),
+}
+
+for m in modelos:
+    modelo = WrapperModelo(m, modelos[m])
+    # Entrenar unicamente utilizando los espectros normalizados (x) y composiciones normalizadas (y) completos
+    modelo.fit(X, y)
+    modelo.ejecutar()
+    # modelo.graficar()  
